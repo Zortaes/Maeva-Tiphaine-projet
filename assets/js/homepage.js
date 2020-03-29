7a113ed2e66317556af6fe93221072a34d274446
MaevaSimao <mae.simao6473@gmail.com>
2020-03-29 01:16:57 +0100

    premier carousel ok, manque responsive

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -8,55 +8,23 @@ import '../css/homepage.scss';
 var homepage = {
 
    // property for matchMedia min-width:575.98px
-   breakSmall: window.matchMedia("(min-width: 575.98px)"),
+   breakSmall: window.matchMedia("(min-width: 767.98px)"),
 
 
     /**
      * init homepage
      */
     init:function() { 
+
       console.log('init homepage');
-
-
-      let $firstElement = $('carousel-item:first	').addClass('active'); 
-      console.log($firstElement); 
-
-
-      //$(document).ready(function() {
-      //  $("#myCarousel").on("slide.bs.carousel", function(e) {
-      //    var $e = $(e.relatedTarget);
-      //    var idx = $e.index();
-      //    var itemsPerSlide = 3;
-      //    var totalItems = $(".carousel-item").length;
-      //
-      //    if (idx >= totalItems - (itemsPerSlide - 1)) {
-      //      var it = itemsPerSlide - (totalItems - idx);
-      //      for (var i = 0; i < it; i++) {
-      //        // append slides to end
-      //        if (e.direction == "left") {
-      //          $(".carousel-item")
-      //            .eq(i)
-      //            .appendTo(".carousel-inner");
-      //        } else {
-      //          $(".carousel-item")
-      //            .eq(0)
-      //            .appendTo($(this).find(".carousel-inner"));
-      //        }
-      //      }
-      //    }
-      //  });
-      //});
-     //
-    //
-
+ 
     
       if (homepage.breakSmall.matches) { 
       console.log('match small'); 
-      let $elements = $('.carousel-item').length; 
-      let $secondElement = $('.carousel-item').next(); 
-     console.log($secondElement); 
-      
-      //$secondElement.addClass('active'); 
+        
+      $('#mobile-slide').remove(); 
+      $('#mobile-slide-2').remove(); 
+ 
       }
 
 
@@ -68,4 +36,3 @@ var homepage = {
 
 $(homepage.init);
 
-console.log('ta soeur')
