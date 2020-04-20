3f7e150ca85ceae9319be824ecd667c3bb76dedd
Zortaes <surygalatiphaine@googlemail.com>
2020-04-20 17:32:53 +0100

    pagination articles by user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,6 +8,7 @@ use App\Entity\User;
 use App\Entity\Article;
 use App\Services\Slugger;
 use App\Form\Type\UserType;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -153,12 +154,16 @@ class UserController extends AbstractController
      * @param User $user that we want sho all his articles
      * @return Articles by user
      */
-    public function articleByUser(User $user)
+    public function articleByUser(User $user, PaginatorInterface $paginator, Request $request)
     {
 
-        $articles = $this->getDoctrine()->getRepository(Article::class)->findBy([
-            "user" => $user
-        ]);
+        $articles = $paginator->paginate
+        (
+            $this->getDoctrine()->getRepository(Article::class)->findBy(["user" => $user]), // Requête contenant les données à paginer (ici nos articles)
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            6// Nombre de résultats par page
+        );
+
 
         return $this->render('user/article_by_user.html.twig',
         [
