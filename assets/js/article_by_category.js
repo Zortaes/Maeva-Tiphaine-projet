aca43095d140e1398b6738233b56863bafade1d9
MaevaSimao <mae.simao6473@gmail.com>
2020-05-08 17:37:01 +0200

    pagination css en cours

--- assets/js/article_by_category.js
+++ assets/js/article_by_category.js
@@ -1,1 +1,25 @@
 import '../css/article_by_category.scss';
+
+
+
+/* 1 */
+var $firstItemPagination = $('.pagination span:first-child'); 
+
+/* 1 */
+$firstItemPagination.css({
+    borderTopLeftRadius : '12px 12px', 
+    borderBottomLeftRadius : '12px 12px'
+   
+});
+
+/* 2 */
+var $lastItemPagination = $('.pagination span:last-child');
+
+/* 2 */
+$lastItemPagination.css({
+    borderTopRightRadius : '12px 12px', 
+    borderBottomRightRadius : '12px 12px', 
+    borderRight : 'none'
+   
+});
+
