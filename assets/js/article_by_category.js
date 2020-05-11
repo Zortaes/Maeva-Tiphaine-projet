564da74b42d47ed4bf41ea3408bde69d98de1c25
MaevaSimao <mae.simao6473@gmail.com>
2020-05-11 17:25:00 +0200

    pagination css ok

--- assets/js/article_by_category.js
+++ assets/js/article_by_category.js
@@ -1,25 +1,8 @@
 import '../css/article_by_category.scss';
 
 
+var $itemPrevious = $('.previous a'); 
+$itemPrevious.text('Retour');
 
-/* 1 */
-var $firstItemPagination = $('.pagination span:first-child'); 
-
-/* 1 */
-$firstItemPagination.css({
-    borderTopLeftRadius : '12px 12px', 
-    borderBottomLeftRadius : '12px 12px'
-   
-});
-
-/* 2 */
-var $lastItemPagination = $('.pagination span:last-child');
-
-/* 2 */
-$lastItemPagination.css({
-    borderTopRightRadius : '12px 12px', 
-    borderBottomRightRadius : '12px 12px', 
-    borderRight : 'none'
-   
-});
-
+var $itemNext = $('.next a'); 
+$itemNext.text('Suivant');
