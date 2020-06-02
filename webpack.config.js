e3eddbb60dc87a534e23e53e53a5c46e77baad6b
MaevaSimao <mae.simao6473@gmail.com>
2020-06-02 19:48:52 +0200

    form js et css trier, verification fichier js et css de tous les form ok

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,9 @@ Encore
     .addEntry('pagination', './assets/js/pagination.js')
     .addEntry('error', './assets/js/error.js')
     .addEntry('formSignup', './assets/js/formSignup.js')
+    .addEntry('formLogin', './assets/js/formLogin.js')
+    .addEntry('formArticle', './assets/js/formArticle.js')
+
    
 
     //.addEntry('page1', './assets/js/page1.js')
