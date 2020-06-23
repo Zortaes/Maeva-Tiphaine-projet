617d46bc8352e74a47ab49d9c93629ee3e77e1ac
MaevaSimao <mae.simao6473@gmail.com>
2020-06-23 16:45:24 +0200

    user delete avatar ok FRONT, and BACK

--- assets/js/profil.js
+++ assets/js/profil.js
@@ -5,7 +5,7 @@ import 'jquery-cropper/dist/jquery-cropper.min.js';
 import 'bootstrap/js/dist/modal';
 import * as Cropper from '../../vendor/presta/image-bundle/Resources/public/js/cropper.js';
 
-$(".cropper-local").removeClass('col-4').addClass('col-8 col-sm-6');
+$(".cropper-local").removeClass('col-4').addClass('col-12 col-xl-7 cropper-local-custum');
 
 $(".close").addClass('col-2 col-lg-1');
 
@@ -16,8 +16,21 @@ $('.w-25').addClass('showUpload');
 if($('.cropper-canvas-container').children().attr('src') == "/uploads/user/avatar/defaultAvatar.jpg")
 {
     $('.cropper-canvas-container').hide();
+
+    $('.delete').removeClass('delete'); 
+    
+    $(".cropper-local").removeClass('cropper-local-custum').addClass("mb-4 mt-3 col-sm-6"); 
 }
 
+
+/* add btn "supprimer"  btn "importer" */
+var $btnParent = $(".cropper-local").parent(".row");
+var $btnDelete = $(".delete"); 
+
+$(".btn-delete").addClass("w-75"); 
+$btnDelete.addClass('col-12 col-xl-7 mx-auto'); 
+
+$btnDelete.prependTo($btnParent); 
 
 (function(w, $) {
 
